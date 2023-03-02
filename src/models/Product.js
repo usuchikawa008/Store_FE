599fb668c51a877fc72c0342524610957835ee0e
Tien Thi Nguyen <tiennt5@ocb.com.vn>
2023-03-02 20:36:14 +0700

    Handle home page

---
+++ src/models/Product.js
@@ -0,0 +1,10 @@
+export class Product{
+  constructor(id,name, imagePath, title, price) {
+    this.id = id;
+    this.name = name;
+    this.imagePath = imagePath;
+    this.title = title;
+    this.price = price;
+  }
+}
+export default Product;
